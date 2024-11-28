78059aa0f896a311d3f2823613548976af0a7c6d
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-11-28 19:09:42 -0300

    exercicio módulo 8

---
+++ src/Calculo/Calcular.java
@@ -0,0 +1,17 @@
+package Calculo;
+
+public class Calcular { 
+    public static void main(String args[]) {
+        int num1 = 10;
+        int num2 = 8;
+        int num3 = 6;
+        int num4 = 4;
+
+        int soma = num1 + num2 + num3 + num4;
+
+        int media = soma / 4;
+
+        System.out.println("Sua média é: " + media);
+    }
+}
+
