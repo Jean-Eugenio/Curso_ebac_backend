04e6718945549697efb14b8039bb615a68198bea
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-12-19 19:21:25 -0300

    exercicio módulo 22

---
+++ src/genero/Demo.java
@@ -0,0 +1,49 @@
+package genero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        
+        List<Pessoa> pessoas = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean fluxo = true;
+
+        while (fluxo) {
+            System.out.println("Deseja adicionar uma pessoa na lista? [S/N] ");
+            String resp = sc.nextLine();
+            if (resp.equalsIgnoreCase("N")) {
+                fluxo = false;
+                break;
+            }
+
+            System.out.println("Digite o nome: ");
+            String nome = sc.nextLine();
+
+
+            System.out.println("Digite o sexo [Masculino/Feminino]: ");
+            String sexo = sc.nextLine();
+
+            Pessoa novaPessoa = new Pessoa(nome, sexo);
+            pessoas.add(novaPessoa);
+        }
+        sc.close();
+
+        boolean naoPossuiMulheres = pessoas.stream()
+                .noneMatch(pessoa -> pessoa.getSexo().equalsIgnoreCase("Feminino"));
+
+        if (naoPossuiMulheres) {
+            System.out.println("NÃO POSSUEM MULHERES NA LISTA!");
+        } else {
+            System.out.println("***AS PESSOAS NA LISTA DO SEXO FEMININO SÃO: *****");
+            pessoas.stream()
+                    .filter(pessoa -> pessoa.getSexo().equalsIgnoreCase("Feminino"))
+                    .forEach(pessoa -> System.out.println(pessoa.getNome()));
+
+        }
+    }
+}
