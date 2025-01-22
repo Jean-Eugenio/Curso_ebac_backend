4ffbbce6f1b9cd3b58b2196ebcd7cee5a98fe9ad
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2025-01-22 20:01:41 -0300

    Exercicio módulo 42

---
+++ ebac_modulo42/ProdutoServiceAlunos/src/main/java/br/com/jean/vendas/online/errorhandling/ApiSubError.java
@@ -0,0 +1,5 @@
+package br.com.jean.vendas.online.errorhandling;
+
+public class ApiSubError {
+
+}
---
+++ ebac_modulo42/VendaServiceAlunos/src/main/java/br/com/jean/vendas/online/usecase/BuscaVenda.java
@@ -0,0 +1,31 @@
+package br.com.jean.vendas.online.usecase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.jean.vendas.online.domain.Venda;
+import br.com.jean.vendas.online.repository.IVendaRepository;
+
+@Service
+public class BuscaVenda {
+
+	private IVendaRepository vendaRepository;
+	
+	@Autowired
+	public BuscaVenda(IVendaRepository produtoRepository) {
+		this.vendaRepository = produtoRepository;
+	}
+	
+	public Page<Venda> buscar(Pageable pageable) {
+		return vendaRepository.findAll(pageable);
+	}
+
+	public Venda buscarPorCodigo(String codigo) {
+		return vendaRepository.findByCodigo(codigo)
+				.orElseThrow();
+	}
+
+	
+}
